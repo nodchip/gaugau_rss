5adbceedc1aafadeabeadac4c4e91084a0f46459
nodchip <nodchip@gmail.com>
2025-05-16 16:21:20 +0900

    host='0.0.0.0'を指定した。

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def rss_feed(work_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
